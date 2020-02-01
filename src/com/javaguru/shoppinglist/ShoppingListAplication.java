ca22bbfb49fa24b5238669c85dd9f7ff334c8d0f
ksotskis <ksotskis@gmail.com>
2020-02-01 21:55:09 +0200

    Single Responsibility Principle

---
+++ src/com/javaguru/shoppinglist/ShoppingListAplication.java
@@ -0,0 +1,11 @@
+package com.javaguru.shoppinglist;
+
+import com.javaguru.shoppinglist.console.ConsoleUI;
+
+public class ShoppingListAplication {
+
+    public static void main(String[] args) {
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.execute();
+    }
+}
