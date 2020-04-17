0f38bc00f621f7e158f94ead01f821c92a42a025
ksotskis <ksotskis@gmail.com>
2020-04-17 21:27:09 +0300

    test

--- src/com/javaguru/shoppinglist/service/validation/ProductValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.javaguru.shoppinglist.service.validation;
-
-public class ProductValidationException extends RuntimeException {
-
-    public ProductValidationException(String message) {
-        super(message);
-    }
-}
